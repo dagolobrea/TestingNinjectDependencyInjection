c5beecb611c5c83ef07829b8c5149273f26028fd
dagolobrea <dagolobrea@gmail.com>
2014-03-26 14:35:11 +0100

    Subida base

---
+++ TestingNinjectDependencyInjection/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestingNinjectDependencyInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FormHandler formHandler = new FormHandler();
+            formHandler.Handle("test@test.es");
+
+            Console.ReadLine();
+        }
+    }
+
+    public class MailSender
+    {
+        public void Send(string adress, string subject)
+        {
+            Console.WriteLine("Send mail to [{0}] with subject [{1}]", adress, subject);
+        }
+    }
+
+    public class FormHandler
+    {
+        public void Handle(string adress)
+        {
+            MailSender sender = new MailSender();
+            sender.Send(adress, "Ejemplo non-Ninject");
+        }
+    }
+
+}
